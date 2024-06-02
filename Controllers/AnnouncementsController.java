738321c721e82b369813271d55e58317c7b8257a
AnTheod <164797383+AnTheod@users.noreply.github.com>
2024-06-02 21:29:53 +0300

    Create AnnouncementsController.java

---
+++ Controllers/AnnouncementsController.java
@@ -0,0 +1,14 @@
+package billGuru.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AnnouncementsController {
+
+	@RequestMapping (method = RequestMethod.GET, value = "/announcements") 
+	public String announcements() {
+		return "announcements";
+	}
+}
